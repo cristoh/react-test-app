4d5ef26bcc091032a716940e016ea616fc89d3fa
r34ct <cristian.tohatan@gmail.com>
2018-03-29 13:53:27 -0400

    replaced src folder with weather api code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,92 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Form from './components/Form';
+import Titles from './components/Titles';
+import Weather from './components/Weather';
 import './App.css';
 
+const API_KEY = 'f22e51c1fa47802615273fd8abd3ebfc';
+
 class App extends Component {
+  /* In react 15, would do this:
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+   This has been deprecated in React 16.
+   Still supported in 16 though.
+*/
+
+// React 16 definition of State
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+
+  getWeather = async (e) => {    
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+
+    //converting api call to json data and assigning to a constant
+    const data = await api_call.json();
+    if (city && country){
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      })
+      console.log(data);
+    }
+    else{
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity:undefined,
+        description: undefined,
+        error: "Please enter the values"
+      })
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React CHANGED!!!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form  getWeather={this.getWeather}/>
+                  <Weather 
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Titles extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="title-container__title">Weather Finder With Changes!</h1>
+                <h2 className="title-container__subtitle">Find weather around the world...</h2>
+            </div>
+        );
+    }
+}
+
+export default Titles;
